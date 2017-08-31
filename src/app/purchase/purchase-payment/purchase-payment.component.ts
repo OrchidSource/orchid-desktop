d21650b2dbacb7d78159e5f0b7f0f7cd1b380d11
Alex Bundy <abundy@opuslogica.com>
2017-08-31 15:36:35 -0700

    feat(purchase): Select payment method; styling, including "chiclets" on the buttons.

--- src/app/purchase/purchase-payment/purchase-payment.component.ts
+++ src/app/purchase/purchase-payment/purchase-payment.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+const PTYPES = {
+  CREDIT_CARD: 'CREDIT_CARD',
+  PAYPAL: 'PAYPAL',
+  CRYPTOCURRENCY: 'CRYPTOCURRENCY'
+}
+
 @Component({
   selector: 'app-purchase-payment',
   templateUrl: './purchase-payment.component.html',
@@ -7,6 +13,11 @@ import { Component, OnInit } from '@angular/core';
   host: {class: 'modal-host'}
 })
 export class PurchasePaymentComponent implements OnInit {
+
+  PAYMENT_TYPES = PTYPES;
+  form = {
+    payment_type: PTYPES.CREDIT_CARD
+  }
 
   constructor() { }
 
