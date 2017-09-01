c22bc1d67f40662eeeea3e0b83fdd334a34460ad
Alex Bundy <abundy@opuslogica.com>
2017-09-01 13:57:03 -0700

    feat(purchase form): Form validation; WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,6 +25,7 @@ import { PurchasePaymentComponent } from './purchase/purchase-payment/purchase-p
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     NgbModule.forRoot()
   ],
--- src/app/purchase/purchase-payment/purchase-payment.component.ts
+++ src/app/purchase/purchase-payment/purchase-payment.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { PurchaseCart } from '../purchase-cart';
 
+// TODO: we've got enums now; use those
 const PTYPES = {
   CREDIT_CARD: 'CREDIT_CARD',
   PAYPAL: 'PAYPAL',
@@ -17,13 +19,29 @@ export class PurchasePaymentComponent implements OnInit {
 
   PAYMENT_TYPES = PTYPES;
   token_purchase_cart = PurchaseCart;
-  form = {
-    payment_type: PTYPES.CREDIT_CARD
+
+  payment_form: FormGroup;
+
+  constructor() {
+    this.build_form();
   }
 
-  constructor() { }
+  requiredFactory = function(payment_type: string) {
+    return function(control: AbstractControl) {
+      debugger;
+      return payment_type === this.payment_form.get('payment_type') && control.value.length === 0 ? {er: 'required'} : null;
+    }
+  }
+
+  build_form() {
+    this.payment_form = new FormGroup({
+      'credit_card_name': new FormControl('', this.requiredFactory(PTYPES.CREDIT_CARD)),
+      'payment_type': new FormControl(PTYPES.CREDIT_CARD)
+    });
+  }
 
   ngOnInit() {
+
   }
 
 }
