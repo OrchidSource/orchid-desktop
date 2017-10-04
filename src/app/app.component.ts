268b22148ee5c24f0fbf36a78d0ef8d2b4db4be3
Jackie Peters <jackiep@meshlabs.org>
2017-10-04 14:55:39 -0700

    New connect button and functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,19 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 @Component({
   selector: 'body',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 isDB = true;
-connected = false;
+status = 'Connect';
 imgsrc='assets/imgs/button-not-connected.png';
   title = 'app';
+}
+
+function Ctrl($scope) {
+    $scope.state = 'on';
+
+    $scope.changeState = function() {
+        $scope.state = $scope.state === 'on' ? 'off' : 'on';
+    }
 }
