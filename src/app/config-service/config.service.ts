0b5db9929645bd6144e9451921fb2ce6ad8d9aa7
will <wlbberman@gmail.com>
2017-10-15 02:19:40 -0700

    browsing locations are now programmatic as defined by classes/browsing-location and config-service

---
+++ src/app/config-service/config.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+
+import { BrowsingLocation } from '../classes/browsing-location'
+
+@Injectable()
+export class ConfigService {
+
+    constructor() {
+        this._blankBrowsingLocation = new BrowsingLocation("", "");
+        this._browsingLocations = [];
+        this._initBrowsingLocations()
+    }
+
+    private _selectedBrowsingLocation : BrowsingLocation;
+
+    private _blankBrowsingLocation : BrowsingLocation;
+
+    private _browsingLocations : BrowsingLocation[];
+
+    private _availableBrowsingLocations : any[] = [
+        {
+        nick: "de",
+        name: "Germany",
+    },
+        {
+        nick: "jp",
+        name: "Japan",
+    },
+        {
+        nick: "gb",
+        name: "United Kingdom",
+    },
+        {
+        nick: "us",
+        name: "The United States"
+    }]
+
+    private _initBrowsingLocations() {
+        this._availableBrowsingLocations.forEach((bl) => {
+            this._browsingLocations.push(new BrowsingLocation(bl.nick, bl.name));
+        })
+        this._selectedBrowsingLocation = this._browsingLocations[0];
+    }
+
+    get selectedBrowsingLocation() : BrowsingLocation {
+        if (this._selectedBrowsingLocation === undefined) {
+            return new BrowsingLocation("us", "lksadjf")
+            //return this._blankBrowsingLocation;
+        }
+        return this._selectedBrowsingLocation;
+    }
+
+    get browsingLocations() : BrowsingLocation[] {
+        return this._browsingLocations;
+    }
+
+    set selectedBrowsingLocation(browsingLocation : BrowsingLocation) {
+        this._selectedBrowsingLocation = browsingLocation; 
+    }
+
+
+}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,15 +3,35 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { BrowserModule } from "@angular/platform-browser";
 import { status } from "../app.component";
 
+import { ConfigService } from "../config-service/config.service";
+
+import { BrowsingLocation } from "../classes/browsing-location";
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 
-
-
 export class DashboardComponent implements OnInit {
+
+    constructor(private _config : ConfigService) {
+    }
+
     ngOnInit() {
-     }
+    }
+
+    setSelectedBrowsingLocation(browsingLocation : BrowsingLocation) {
+        this._config.selectedBrowsingLocation = browsingLocation;
+    }
+
+    selectedBrowsingLocation() : BrowsingLocation {
+        console.log(this._config.selectedBrowsingLocation.mapUrl);
+        return this._config.selectedBrowsingLocation;
+    }
+
+    browsingLocations() : BrowsingLocation[] {
+        return this._config.browsingLocations;
+    }
+
 }
