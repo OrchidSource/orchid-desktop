cfe6e86fc4cc45ce6a3d9cad5853bfcb324bd282
Alex Bundy <abundy@opuslogica.com>
2018-04-06 15:35:03 -0700

    Translate transaction type

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletComponent implements OnInit {
 
-  constructor() { }
+  transactions: Array<object> = [];
+  constructor() {}
 
   ngOnInit() {
+    this.getTransactions();
+  }
+
+  getTransactions() {
+    // mock up transaction data for now
+
+    var today = new Date();
+
+    for (let i = 0; i < 10; i ++) {
+      this.transactions.push({
+        from: 'X09HS7GHFFIDXIANGIA',
+        to: 'F10HW40HFFIGXIACQIA',
+        status: (i % 2) ? 'sent' : 'received',
+        amount: 1 + i * 10.25,
+        date: new Date(today.getFullYear(), today.getUTCMonth(), today.getUTCDay() - i)
+      })
+    }
+
   }
 
 }
