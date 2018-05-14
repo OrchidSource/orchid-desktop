eb5e258a9eb303ebdbb126382208dd98367475f4
Alex Bundy <abundy@opuslogica.com>
2018-05-14 11:19:34 -0700

    Advanced settings styling. Misc other styling.

--- src/app/settings/settings-advanced/settings-advanced.component.ts
+++ src/app/settings/settings-advanced/settings-advanced.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsAdvancedComponent implements OnInit {
 
-  setings: object = {};
-  
+  settings: object = {
+    PRICE_GB_USED: 0.569423,
+    PRICE_GB_SHARED: 0.76942
+  };
+
   constructor() { }
 
   ngOnInit() {
