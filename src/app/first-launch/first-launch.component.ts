e3477a46e04983d014cb8bb8625dbf3c6491a53d
Alex Bundy <abundy@opuslogica.com>
2018-05-19 16:40:20 -0700

    Update first-launch: Dots look better. Dots are clickable. Navigation with arrow keys.

--- src/app/first-launch/first-launch.component.ts
+++ src/app/first-launch/first-launch.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+
+enum KEY_CODE {
+  RIGHT_ARROW = 39,
+  LEFT_ARROW = 37
+}
 
 @Component({
   selector: 'app-first-launch',
@@ -11,6 +16,17 @@ export class FirstLaunchComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if (event.keyCode === KEY_CODE.RIGHT_ARROW && this.page < 3) {
+      this.page++;
+    }
+
+    if (event.keyCode === KEY_CODE.LEFT_ARROW && this.page > 1) {
+      this.page--;
+    }
   }
 
 }
