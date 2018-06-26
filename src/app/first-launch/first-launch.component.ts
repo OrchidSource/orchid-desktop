9f19b6f456de626e7af90596f593d01004a349ff
Alex Bundy <abundy@opuslogica.com>
2018-06-26 16:41:43 -0700

    Add video for first-run. WIP.

--- src/app/first-launch/first-launch.component.ts
+++ src/app/first-launch/first-launch.component.ts
@@ -1,10 +1,6 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
-
-enum KEY_CODE {
-  RIGHT_ARROW = 39,
-  LEFT_ARROW = 37
-}
 const LAST_LOCATION_LOCALSTORAGE_KEY = "FIRST_RUN_LAST_LOCATION";
 
 @Component({
@@ -12,51 +8,22 @@ const LAST_LOCATION_LOCALSTORAGE_KEY = "FIRST_RUN_LAST_LOCATION";
   templateUrl: './first-launch.component.html',
   styleUrls: ['./first-launch.component.scss']
 })
-export class FirstLaunchComponent implements OnInit {
+export class FirstLaunchComponent {
 
-  /** The page number */
-  public page: number = 1;
-  constructor() {}
+  constructor(private router: Router) { }
 
-  ngOnInit() {
-    var lastPage = this.getLastPage();
-    if (lastPage) {
-      this.page = lastPage;
-    }
+  videoStopped() {
+    console.log('stopped');
+
+    setTimeout(() => {
+      this.firstRunDone();
+    }, 2000);
   }
 
-  changePage (page) {
-    this.page = page;
-
-    var lastPage = this.getLastPage();
-
-    if (!lastPage || (lastPage && this.page > Number(lastPage))) {
-      localStorage.setItem(LAST_LOCATION_LOCALSTORAGE_KEY, page);
-    }
-  }
 
   firstRunDone() {
     localStorage.setItem(LAST_LOCATION_LOCALSTORAGE_KEY, 'DONE');
-  }
-
-  private getLastPage() {
-    var page = localStorage.getItem(LAST_LOCATION_LOCALSTORAGE_KEY);
-    if (page) {
-      return Number(page);
-    } else {
-      return null;
-    }
-  }
-
-  @HostListener('window:keyup', ['$event'])
-  keyEvent(event: KeyboardEvent) {
-    if (event.keyCode === KEY_CODE.RIGHT_ARROW && this.page < 3) {
-      this.page++;
-    }
-
-    if (event.keyCode === KEY_CODE.LEFT_ARROW && this.page > 1) {
-      this.page--;
-    }
+    this.router.navigate(['/main/dashboard']);
   }
 
 }
